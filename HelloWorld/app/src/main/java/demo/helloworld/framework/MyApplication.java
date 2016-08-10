5ee78bb7d27270e61018b086d6483cf265beda26
wangdong <wangdong_0428@163.com>
2016-08-10 08:42:01 +0800

    sourcetree 发生错误

--- HelloWorld/app/src/main/java/demo/helloworld/framework/MyApplication.java
+++ HelloWorld/app/src/main/java/demo/helloworld/framework/MyApplication.java
@@ -11,6 +11,7 @@ import org.xutils.x;
  * Created by wangdong on 16/8/3.
  */
 public class MyApplication extends Application {
+
     @Override
     public void onCreate() {
         super.onCreate();
